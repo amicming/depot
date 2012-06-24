3b15c278a7adf68e92c6b376c7603c9186f0f59b
Dynamic Programming <amicming@gmail.com>
2012-06-24 16:18:24 -0700

    missed some files in First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '780ed47e2bd3d8fc607eb7852a805431b9c12d7c93ef40c30e65c3db085fae034a5d1479cd52cf3048fc4d5e2b80ad41ccb9f06c0224e5f46ea51e6e2b5b8773'
