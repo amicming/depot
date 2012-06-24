9c7c27c1a50e20839ea3d025de887e83c3fa17fc
Dynamic Programming <amicming@gmail.com>
2012-06-24 16:36:59 -0700

    Fixed Heroku bug, remove sqlite3, add pg

--- db/schema.rb
+++ db/schema.rb
@@ -22,4 +22,16 @@ ActiveRecord::Schema.define(:version => 20120624223714) do
     t.datetime "updated_at",                                :null => false
   end
 
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+    t.string   "remember_token"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+
 end
