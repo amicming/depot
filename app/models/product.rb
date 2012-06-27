ec5c01ba5275d0a956b438ebb4b8540f5023ac43
Dynamic Programming <amicming@gmail.com>
2012-06-26 19:17:42 -0700

    add playtime validation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ActiveRecord::Base
 	attr_accessible :description, :image_url, :price, :title
 
-	validates :title, :description, :image_url, presence: true
+	validates :title, :description, :image_url, presence: true, :length => {
+		:minimum => 10,
+		:message => 'must be at least ten characters long'
+	}
 	validates :price, numericality: { greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
